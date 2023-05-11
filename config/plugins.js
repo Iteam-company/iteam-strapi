b52c4d2e3bd322d0cab53a997824eea3eed546ec
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-05-12 00:30:12 +0300

    cloudinary config

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,17 @@
 module.exports = ({ env }) => ({
-  "strapi-plugin-populate-deep": {
+  upload: {
     config: {
-      defaultDepth: 5, // Default is 5
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
   },
 });
